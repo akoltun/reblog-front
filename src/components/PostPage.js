287518d099cde2a30ce7c857614518373120957c
Alexander Koltun <alexander.koltun@gmail.com>
2017-01-29 17:55:00 +0100

    Add request to get post item from the server for PostPage

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -4,14 +4,43 @@ import { Item, Divider } from 'semantic-ui-react';
 
 import BlogItem from 'components/widgets/blog/Item';
 
-import { items } from 'constants/static/items';
+import request from 'superagent';
+// import { items } from 'constants/static/items';
 
-const PostPage = ({params}) => (
-  <Item.Group>
-    <BlogItem item={items[params.id - 1]} />
-    <Divider />
-  </Item.Group>
-);
+class PostPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: {} };
+  }
+
+  componentDidMount() {
+    this.fetchPost(this.props.params.id - 1);
+  }
+
+  fetchPosts(callback) {
+    request.get(
+      'http://localhost:3002/',
+      {},
+      (err, res) => callback(err, res)
+    );
+  }
+
+  fetchPost(id) {
+    this.fetchPosts(
+      (err, res) => this.setState({ item: res.body[id] })
+    );
+  }
+
+  render() {
+    const {item} = this.state;
+    return (
+      <Item.Group>
+        <BlogItem item={item} />
+        <Divider />
+      </Item.Group>
+    );
+  }
+}
 
 PostPage.propTypes = {
   params: PropTypes.object
