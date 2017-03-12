72bf2b86dac738458f3cc3de587ee2e013f0a0d2
Alexander Koltun <alexander.koltun@gmail.com>
2017-03-12 16:09:09 +0100

    Refactor Like action to use middleware

--- src/actions/Like.js
+++ src/actions/Like.js
@@ -1,31 +1,19 @@
-import request from 'superagent';
-
-import { API_ROOT } from 'constants/API';
 import * as types from 'constants/actionTypes/LikePostActionTypes';
+import { API_CALL } from 'middleware/API';
 import { likePostPath } from 'helpers/routes';
 
-const sendLike = (id) => ({
-  type: types.SEND_LIKE_REQUEST,
-  id
-});
-
-const succeedLike = (response) => ({
-  type: types.SEND_LIKE_SUCCESS,
-  response
-});
-
-const errorLike = () => ({
-  type: types.SEND_LIKE_ERROR
-});
-
 export function likePost(id) {
-  return (dispatch) => {
-    dispatch(sendLike(id));
-
-    return request
-      .post(`${API_ROOT}${likePostPath(id)}`)
-      .end((err, res) => {
-        err ? dispatch(errorLike()) : dispatch(succeedLike(res.body));
-      });
+  return {
+    [API_CALL]: {
+      endpoint: likePostPath(id),
+      method: 'POST',
+      query: {},
+      types: {
+        request: types.SEND_LIKE_REQUEST,
+        success: types.SEND_LIKE_SUCCESS,
+        errors: types.SEND_LIKE_ERROR
+      }
+    },
+    id
   };
 }
