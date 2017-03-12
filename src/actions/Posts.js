f254b65ce6b46c5775ba2cca2278bf5abb509a0a
Alexander Koltun <alexander.koltun@gmail.com>
2017-03-12 15:21:30 +0100

    Move actual API calls from action creators to middleware

--- src/actions/Posts.js
+++ src/actions/Posts.js
@@ -1,35 +1,17 @@
-import request from 'superagent';
-
-import { API_ROOT } from 'constants/API';
 import * as types from 'constants/actionTypes/PostsActionTypes';
-import { postsPath } from 'helpers/routes';
-
-const requestPosts = () => ({
-  type: types.FETCH_POSTS_REQUEST
-});
-
-const receivePosts = (response) => ({
-  type: types.FETCH_POSTS_SUCCESS,
-  response
-});
-
-const errorPosts = () => ({
-  type: types.FETCH_POSTS_ERROR
-});
-
-export const syncPostToPosts = (post) => ({
-  type: types.SYNC_POST_TO_POSTS,
-  post
-});
+import { API_CALL } from 'middleware/API';
 
 export function fetchPosts() {
-  return (dispatch) => {
-    dispatch(requestPosts());
-
-    return request
-      .get(`${API_ROOT}${postsPath()}`)
-      .end((err, res) => {
-        err ? dispatch(errorPosts()) : dispatch(receivePosts(res.body));
-      });
+  return {
+    [API_CALL]: {
+      endpoint: '/',
+      method: 'GET',
+      query: {},
+      types: [
+        types.FETCH_POSTS_REQUEST,
+        types.FETCH_POSTS_SUCCESS,
+        types.FETCH_POSTS_ERROR
+      ]
+    }
   };
 }
