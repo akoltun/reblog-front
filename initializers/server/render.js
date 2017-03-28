38b39cd3b3072d012a4c2c3aae1f05a148607ce5
Alexander Koltun <alexander.koltun@gmail.com>
2017-03-28 23:00:13 +0200

    Add 404 and 500 error handlers

--- initializers/server/render.js
+++ initializers/server/render.js
@@ -13,26 +13,42 @@ import prepareData from 'helpers/prepareData';
 
 const store = createStore();
 
+const notFound = { status: 404, body: 'NOT FOUND' };
+const serverError = msg => ({ status: 500, body: msg });
+
+const sendResponse = (res, obj) => res.status(obj.status).send(obj.body);
+
 export default (req, res) => {
-  match({ routes, location: req.url }, (error, redirectLocation, renderProps) =>
-    Promise.all(compact(prepareData(store, renderProps))).then(() => {
-      const initialState = JSON.stringify(store.getState());
+  match({ routes, location: req.url },
+    (error, redirectLocation, renderProps) => {
+      if (error) return sendResponse(res, serverError(error.message));
+      if (redirectLocation) {
+        res.redirect(redirectLocation.pathname + redirectLocation.search);
+        return;
+      }
+      if (renderProps === undefined) return sendResponse(res, notFound);
 
-      const content = ReactDOMServer.renderToString(
-        React.createElement(
-          Provider,
-          { store },
-          React.createElement(RouterContext, renderProps)
-        )
-      );
+      return Promise.all(compact(prepareData(store, renderProps))).then(() => {
+        const initialState = JSON.stringify(store.getState());
 
-      const head = Helmet.rewind();
+        const content = ReactDOMServer.renderToString(
+          React.createElement(
+            Provider,
+            { store },
+            React.createElement(RouterContext, renderProps)
+          )
+        );
 
-      res.status(200);
-      res.render(
-        'index',
-        { initialState, content, head }
-      );
-    })
+        const head = Helmet.rewind();
+
+        res.status(200);
+        res.render(
+          'index',
+          { initialState, content, head }
+        );
+      })
+      .catch(error => sendResponse(res,
+          error ? serverError(error.message) : notFound));
+    }
   );
 };
